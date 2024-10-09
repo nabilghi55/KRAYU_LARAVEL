2e11cf21fd05fe18c98c273a9db294252d0cf6de
nabilghi55 <nabilghi55@student.ub.ac.id>
2024-10-10 03:03:26 +0700

    app blade

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,62 +1,105 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en_AU" />
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Krayu</title>
-	<meta name="description" content="" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Cattle Market</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
 
-	<meta name="HandheldFriendly" content="True" />
-	<meta name="pinterest" content="nopin" />
-	@vite('resources/css/app.css')
-	<meta property="og:locale" content="en_AU" />
-	<meta property="og:type" content="website" />
-	<meta property="fb:admins" content="" />
-	<meta property="fb:app_id" content="" />
-	<meta property="og:site_name" content="" />
-	<meta property="og:title" content="" />
-	<meta property="og:description" content="" />
-	<meta property="og:url" content="" />
-	<meta property="og:image" content="" />
-	<meta property="og:image:type" content="image/jpeg" />
-	<meta property="og:image:width" content="" />
-	<meta property="og:image:height" content="" />
-	<meta property="og:image:alt" content="" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="pinterest" content="nopin" />
 
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:site" content="" />
-	<meta name="twitter:description" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:image:alt" content="" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta property="og:locale" content="en_AU" />
+    <meta property="og:type" content="website" />
+    <meta property="fb:admins" content="" />
+    <meta property="fb:app_id" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="" />
+    <meta property="og:image:height" content="" />
+    <meta property="og:image:alt" content="" />
+
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:image:alt" content="" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 
-	<link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/slick.css') }}" />
-	<link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/slick-theme.css') }}" />
-	<link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/ion.rangeSlider.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/slick.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/slick-theme.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/ion.rangeSlider.min.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('front-assets/css/style.css') }} " />
 
-	<!-- DataTables -->
-	<link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin-assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&family=Raleway:ital,wght@0,400;0,600;0,800;1,200&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&family=Raleway:ital,wght@0,400;0,600;0,800;1,200&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
 
-	<!-- Fav Icon -->
-	<link rel="shortcut icon" type="image/x-icon" href="{{ asset('img\logo2.png') }}" />
+    <!-- Fav Icon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img\logo2.png') }}" />
 
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body data-instant-intensity="mousedown">
 
+<header class="">
+    <div class="container">
+        <nav class="navbar navbar-expand-xl" id="navbar">
+           
+            <button class="navbar-toggler menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- <span class="navbar-toggler-icon icon-menu"></span> -->
+                  <i class="navbar-toggler-icon fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php" title="Products">Home</a>
+                    </li> -->
+                    @php $categoryCount = 0; @endphp <!-- Initialize a counter -->
+                    @if(getCategories()->isNotEmpty())
+                    @foreach (getCategories() as $category)
+                        @if ($categoryCount < 6) <!-- Check if the counter is less than 5 -->
+                            <li class="nav-item dropdown">
+                                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ $category->name }}
+                                </button>
+                                @if($category->sub_category->isNotEmpty())
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    @foreach ($category->sub_category as $subCategory)
+                                    <li><a class="dropdown-item nav-link" href="{{ route('front.shop', [$category->slug,$subCategory->slug]) }}">{{ $subCategory->name }}</a></li>
+                                    @endforeach
+                                </ul>
+                                @endif
+                            </li>
+                            @php $categoryCount++; @endphp <!-- Increment the counter -->
+                        @endif
+                    @endforeach
+                    @endif
+                </ul>
+            </div>
 
-
+            <div class="right-nav py-0">
+                <a href="{{ route("front.cart") }}" class="ml-3 d-flex pt-2">
+                    <i class="fas fa-shopping-cart text-primary"></i>
+                    <span id="cart-item-count" class="badge badge-danger">0</span>
+                </a>
+            </div>
+        </nav>
+    </div>
+</header>
 
 
 <main>
@@ -64,24 +107,6 @@
 </main>
 
 
-
-<!-- Wishlist Modal -->
-<div class="modal fade" id="wishlistModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-	<div class="modal-dialog">
-	  <div class="modal-content">
-		<div class="modal-header">
-		  <h5 class="modal-title" id="exampleModalLabel">Success</h5>
-		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-		</div>
-		<div class="modal-body">
-
-		</div>
-		<div class="modal-footer">
-		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-		</div>
-	  </div>
-	</div>
-  </div>
 
 <script src="{{ asset('front-assets/js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('front-assets/js/bootstrap.bundle.5.1.3.min.js') }}"></script>
@@ -120,71 +145,51 @@ function myFunction() {
 }
 
 $.ajaxSetup({
-	headers:{
-		'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-	}
+    headers:{
+        'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+    }
 });
 
-	function addToCart(id){
-		$.ajax({
-			url: '{{ route("front.addToCart") }}',
-			type: 'post',
-			data: {id:id},
-			dataType: 'json',
-			success: function(response){
-				console.log(response);
-				if (response.status == true) {
-					// window.location.reload();
-					// window.location.href = '{{ route("front.cart") }}';
-					updateCartItemCount();
-				} else {
-					alert(response.message);
-				}
-			}
-		});
-	}
+    function addToCart(id){
+        $.ajax({
+            url: '{{ route("front.addToCart") }}',
+            type: 'post',
+            data: {id:id},
+            dataType: 'json',
+            success: function(response){
+                console.log(response);
+                if (response.status == true) {
+                    // window.location.reload();
+                    // window.location.href = '{{ route("front.cart") }}';
+                    updateCartItemCount();
+                } else {
+                    alert(response.message);
+                }
+            }
+        });
+    }
 
-	function addToWishlist(id) {
-		$.ajax({
-				url: '{{ route("front.addToWishlist") }}',
-				type: 'post',
-				data: {id:id},
-				dataType: 'json',
-				success: function(response){
-					console.log(response);
-					if (response.status == true) {
+    function addToWishlist(id) {
+        $.ajax({
+                url: '{{ route("front.addToWishlist") }}',
+                type: 'post',
+                data: {id:id},
+                dataType: 'json',
+                success: function(response){
+                    console.log(response);
+                    if (response.status == true) {
 
-						$("#wishlistModal .modal-body").html(response.message)
-						$("#wishlistModal").modal('show');
+                        $("#wishlistModal .modal-body").html(response.message)
+                        $("#wishlistModal").modal('show');
 
-					} else {
-						window.location.href = '{{ route("account.login") }}';
-						//alert(response.message);
-					}
-				}
-			});
-		}
-<style>
-    .bg-olive {
-        background-color: #9FB39A;
-    }
-    .navbar .dropdown-menu {
-        background-color: #9FB39A;
-    }
-    .navbar .nav-link, .navbar .btn, .navbar .btn-outline-dark {
-        color: #1F1F1F;
-    }
-    .navbar .btn-outline-dark:hover {
-        background-color: #7A946E;
-        color: white;
-    }
-    .navbar .btn-outline-dark {
-        border-color: #1F1F1F;
-    }
-	.text-krayu{
-		color: #FFF6E0
-	}
-</style>
+                    } else {
+                        window.location.href = '{{ route("account.login") }}';
+                        //alert(response.message);
+                    }
+                }
+            });
+        }
+
 // Function to update the cart item count
 function updateCartItemCount() {
     $.ajax({
@@ -209,3 +214,4 @@ updateCartItemCount();
 @yield('customJs')
 </body>
 </html>
+
